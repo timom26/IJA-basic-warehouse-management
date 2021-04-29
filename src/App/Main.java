ba86531ae80433f251907426463a0aa12b57d28c
timom26 <xkamen24@stud.fit.vutbr.cz>
2021-04-29 16:20:25 +0200

    prototype - get route to a given shelf and move there

--- src/App/Main.java
+++ src/App/Main.java
@@ -7,22 +7,61 @@
 
 package App;
 
+import Reader.Read;
+import Reader.WarehouseStruct;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import store.ShoppingCart;
+
+import java.util.ArrayList;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader();
+        /*FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setController(new Controller());
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
         primaryStage.setTitle("Warehouse");
         primaryStage.setScene(new Scene(root, 640, 545));
-        primaryStage.show();
+        primaryStage.show();*/
+
+        //init warehouse
+        WarehouseStruct warehouse1 = new WarehouseStruct();
+        Read.ReadWarehousePlan(warehouse1.shelves);
+        warehouse1.setRows(warehouse1.shelves.size());
+        warehouse1.setCols(((ArrayList) warehouse1.shelves.get(0)).size());
+
+
+        ShoppingCart cart1 = new ShoppingCart(warehouse1,2,0);
+        System.out.println("###");
+        cart1.goTo(1,0);
+        System.out.println("###");
+
+        cart1.goTo(1,5);
+        System.out.println("###");
+
+        cart1.goTo(2,5);
+        System.out.println("###");
+
+        cart1.goTo(2,0);
+        System.out.println("###");
+        cart1.goTo(1,3);
+        System.out.println("###");
+
+
+
+        cart1.goTo(0,0);
+        System.out.println(cart1.routeLength(1,0));
+        System.out.println(cart1.routeLength(1,1));
+        System.out.println(cart1.routeLength(2,0));
+        System.out.println(cart1.routeLength(2,1));
+        System.out.println(cart1.routeLength(0,9));
+
+        System.exit(0);
     }
 
 
--- src/store/ShoppingCart.java
+++ src/store/ShoppingCart.java
@@ -5,7 +5,11 @@
  * @brief assignment of java application for basic warehouse management system
  */
 package store;
+import Reader.WarehouseStruct;
+
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ShoppingCart {
@@ -14,14 +18,19 @@ public class ShoppingCart {
     //  #####################  vars  ###############
     private ArrayList<Item> content = new ArrayList<>();
     private int maximumCount = 10;//prepared for future dev with variable max count.
-
-
+    private int coord_x;
+    private int coord_y;
+    private WarehouseStruct warehouse;
+    private boolean engaged = false;//use for calls
     //  ###################  constructor  #################
-    public ShoppingCart(){
-
+    /** warehouse in which the shopping cart is generated,   and starting coords x, y**/
+    public ShoppingCart(WarehouseStruct warehouse,int coord_x,int coord_y){
+        this.coord_x = coord_x;
+        this.coord_y = coord_y;
+        this.warehouse = warehouse;
     }
     //#############  class functions ###########################
-
+    //#####  item manipulation  ############
     /**
      * Fills the cart by any item of a given goods-type from a given shelf
      * @param shelf shelf to take item from
@@ -82,5 +91,162 @@ public class ShoppingCart {
      */
     public int size(){
         return content.size();
+    }
+
+
+
+
+
+
+    //#####  movement functions   #########
+
+
+
+
+
+
+    /**function returns a length of route to a given shelf from current position
+     * @param end_x coord x of the target shelf
+     * @param end_y coord y of the target shelf
+     * @return integer - length of route to a given shelf
+     */
+    public int routeLength(int end_x,int end_y){
+        int start_x = this.coord_x;
+        int start_y = this.coord_y;
+        int warehouse_x = this.warehouse.getCols();
+        int warehouse_y = this.warehouse.getRows();
+
+        //the same column
+        //if on western side and the shelf is in same column (western or eastern)
+        if (start_x % 2 == 1 && (end_x == start_x || end_x == start_x + 1)){
+            return (java.lang.Math.max(start_y,end_y) - java.lang.Math.min(start_y,end_y));
+        }
+        //on the eastern side and the shelf is in the same column (western or eastern)
+        else if (start_x % 2 == 0 && (end_x == start_x || end_x == start_x -1)){
+            return (java.lang.Math.max(start_y,end_y) - java.lang.Math.min(start_y,end_y));
+        }
+        else{
+            //not in same column
+            //calculate whether you go through top or bottom
+            //divided for readability into up/down part, and left/right part
+            int verticalMove = java.lang.Math.min(start_y+end_y + 2,warehouse_y - start_y + warehouse_y - end_y);
+            int horizontalMove = java.lang.Math.abs(end_x - start_x);
+            return verticalMove + horizontalMove;
+        }
+
+    }
+    /**function generates the shortest route to a given shelf and returns
+     * it as a list of 2d points from current position to ending position
+     * @param end_x coord x of the target shelf
+     * @param end_y coord y of the target shelf
+     * @return Route to target shelf - List of 2d points
+     */
+    public List<java.awt.Point> getCoords(int end_x, int end_y){
+        int start_x = this.coord_x;
+        int start_y = this.coord_y;
+        int warehouse_x = this.warehouse.getCols();
+        int warehouse_y = this.warehouse.getRows();
+        List<java.awt.Point> pointList = new ArrayList<>();
+        //the division is the same as when calculating a length of route
+        //when in doubt, consult there. Here, it has been shortened
+        //traded the code readability for a shorter code
+
+        //if in the same col
+        if ((start_x % 2 == 1 && (end_x == start_x || end_x == start_x + 1)) || (start_x % 2 == 0 && (end_x == start_x || end_x == start_x -1)))
+        {//the same col
+            if (end_y > start_y) {
+                for (int i = start_y; i < end_y + 1; i++) {
+                    java.awt.Point newPoint = new Point(start_x, i);
+                    pointList.add(newPoint);
+                }
+            } else {
+                for (int i = start_y; i > end_y - 1; i--) {
+                    java.awt.Point newPoint = new Point(start_x, i);
+                    pointList.add(newPoint);
+                }
+            }
+            if (end_x != start_x) {
+                java.awt.Point newPoint = new Point(end_x, end_y);
+                pointList.add(newPoint);
+            }
+
+        }
+        else
+        {//not in the same col
+            //first up, then down
+            if (start_y + end_y + 2   <   warehouse_y - start_y  + warehouse_y - end_y) {
+
+                //up
+                for (int i = start_y;i>-1;i--){
+                    java.awt.Point newPoint = new Point(start_x,i);
+                    pointList.add(newPoint);
+                }
+                //to the sides
+                if (start_x < end_x){// to the right
+                    for (int i = start_x;i<end_x+1;i++){
+                        java.awt.Point newPoint = new Point(i,-1);
+                        pointList.add(newPoint);
+                    }
+                }
+                else{//to the left
+                    for (int i = start_x;i>end_x-1;i--){
+                        java.awt.Point newPoint = new Point(i,-1);
+                        pointList.add(newPoint);
+                    }
+                }
+
+                //back down
+                for (int i = 0; i< end_y+1;i++){
+                    java.awt.Point newPoint = new Point(end_x,i);
+                    pointList.add(newPoint);
+                }
+            }
+            //first down, then up
+            else{
+                //down
+                for (int i = start_y;i < warehouse_y; i++){
+                    java.awt.Point newPoint = new Point(start_x,i);
+                    pointList.add(newPoint);
+                }
+                //sides
+                if (start_x < end_x) {
+                    //right
+                    for (int i = start_x; i < end_x + 1; i++) {
+                        java.awt.Point newPoint = new Point(i, warehouse_y);
+                        pointList.add(newPoint);
+                    }
+                }
+                else{                    //left
+                    for (int i = warehouse_y - 1; i > end_y - 1; i--) {
+                        java.awt.Point newPoint = new Point(i, warehouse_y);
+                        pointList.add(newPoint);
+                    }
+                }
+
+
+                //back up
+                for (int i = warehouse_y - 1; i > end_y - 1;i--){
+                    System.out.println("back up");
+                    java.awt.Point newPoint = new Point(end_x,i);
+                    pointList.add(newPoint);
+                }
+            }
+        }
+        return pointList;
+    }
+
+    /**function gets route to a shelf, and then makes the cart go to it in real time
+     * @param end_x coord x of the target shelf
+     * @param end_y coord y of the target shelf
+     * @return None
+     */
+    public void goTo(int end_x,int end_y){
+        List<java.awt.Point> coordList = getCoords(end_x,end_y);
+        for (int i = 0; i < coordList.size();i++){
+            System.out.print(coordList.get(i).x+ " " + coordList.get(i).y + " ");
+            this.coord_x = coordList.get(i).x;
+            this.coord_y = coordList.get(i).y;
+        }
+        System.out.println("");
     }
 }
